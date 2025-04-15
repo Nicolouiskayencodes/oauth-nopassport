243afd83413660246d93f6de7d1ddd2c2be9fbb5
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2025-04-14 18:54:39 -0700

    Add env and anchor link to github oauth in front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+// require('dotenv').config();
 import './App.css'
+
+const path='/'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <a href={`https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_clientId}&redirect_uri=${import.meta.env.VITE_callback}?path=${path}&scope=user:email`}>Login with Github</a>
     </>
   )
 }
